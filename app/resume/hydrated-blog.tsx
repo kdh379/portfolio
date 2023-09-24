c4b74751d8a16887f653bfb5af1a119c3238eb84
kdh379 <ehgus37625@naver.com>
2023-09-24 22:21:00 +0900

    tistory 테스트 3차..

--- app/resume/hydrated-blog.tsx
+++ app/resume/hydrated-blog.tsx
@@ -7,13 +7,20 @@ import getQueryClient from "app/query-client";
 import AsyncBoundary from "components/boundary/async-boundary";
 import { getTistoryAccessToken } from "utils/tistory-token";
 
-export default async function HydratedBlog() {
-    // return <AsyncBoundary
-    //     serverErrorFallback={<ErrorFallback />}
-    // >
-    //     <HydratedBlogQuery />
-    // </AsyncBoundary>;
+export default function HydratedBlog() {
 
+    return <AsyncBoundary
+        serverErrorFallback={<ErrorFallback />}
+    >
+        <HydratedBlogQuery />
+    </AsyncBoundary>;
+}
+
+function ErrorFallback( ) {
+    return <div>Tistory API Failed</div>;
+}
+
+async function HydratedBlogQuery() {
     const queryClient = getQueryClient();
     await queryClient.fetchQuery( {
         queryKey: ["tistory.getPost"],
@@ -33,28 +40,3 @@ export default async function HydratedBlog() {
         </Hydrate>
     );
 }
-
-// function ErrorFallback( ) {
-//     return <></>;
-// }
-
-// async function HydratedBlogQuery() {
-//     const queryClient = getQueryClient();
-//     await queryClient.fetchQuery( {
-//         queryKey: ["tistory.getPost"],
-//         queryFn: () => request( {
-//             key: "tistory.getPost",
-//             params: {
-//                 blogName: "gomban",
-//                 access_token: getTistoryAccessToken(),
-//             },
-//         } ),
-//     } );
-//     const dehydratedState = dehydrate( queryClient );
-
-//     return (
-//         <Hydrate state={dehydratedState}>
-//             <Blog />
-//         </Hydrate>
-//     );
-// }
