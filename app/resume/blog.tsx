bfc92ec9c5733dc5557e157eda238fd1226274ee
kdh379 <ehgus37625@naver.com>
2023-09-24 23:02:18 +0900

    Tistory API 빌드 테스트 찐막

--- app/resume/blog.tsx
+++ app/resume/blog.tsx
@@ -1,27 +1,71 @@
 "use client";
 
+import { Hydrate, dehydrate } from "@tanstack/react-query";
+
+import getQueryClient from "app/query-client";
+import AsyncBoundary from "components/boundary/async-boundary";
 import Header from "components/header";
 import Section from "components/section";
 import usePostList from "hooks/post-list";
 import { useResume } from "hooks/resume-list";
+import { api } from "utils/api";
 import { getTistoryAccessToken } from "utils/tistory-token";
 
-export default function Blog( ) {
+const POST_LIST_PARAMS = {
+    blogName: "gomban",
+    access_token: getTistoryAccessToken(),
+};
+
+export default function Blog() {
 
     const { href, icon, title } = useResume( "Blog" );
-
-    const { data } = usePostList( {
-        blogName: "gomban",
-        access_token: getTistoryAccessToken(),
-    } );
-
-    console.log( data );
 
     return <Section id={href}>
         <Header
             headerText={title}
             icon={icon}
         />
-        {data?.tistory.status}
+        <AsyncBoundary
+            serverErrorFallback={<ErrorFallback />}
+        >
+            <HydratedBlog />
+        </AsyncBoundary>
     </Section>;
+}
+
+function ErrorFallback( ) {
+    return <div>Tistory API Failed</div>;
+}
+
+async function HydratedBlog() {
+    const queryClient = getQueryClient();
+    await queryClient.fetchQuery( {
+        queryKey: ["tistory.getPost"],
+        queryFn: () => api( {
+            key: "tistory.getPost",
+            params: {
+                blogName: "gomban",
+                access_token: getTistoryAccessToken(),
+            },
+        } ),
+    } );
+    const dehydratedState = dehydrate( queryClient );
+
+    return (
+        <Hydrate state={dehydratedState}>
+            <BlogPost />
+        </Hydrate>
+    );
+}
+
+function BlogPost( ) {
+
+    const { data } = usePostList( {
+        blogName: "gomban",
+        access_token: getTistoryAccessToken(),
+    } );
+
+    return <>
+        {data?.tistory.status}
+    </>;
 }
