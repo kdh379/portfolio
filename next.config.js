3b342b190e689f8c5bbfd6a0837de55b2cb322c5
kdh379 <ehgus37625@naver.com>
2023-09-24 22:29:36 +0900

    async component 2

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
     /* config options here */
     reactStrictMode: true,
     experimental: {},
+    typescript: {
+        // TODO : Next 에서 Async Server Component 버그가 수정되면 제거
+        // 참조 : https://nextjs.org/docs/app/building-your-application/configuring/typescript#async-server-component-typescript-error
+        ignoreBuildErrors: true,
+    },
 };
 
 module.exports = nextConfig;
